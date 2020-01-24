6f1481fbe799cf50349f3524d317f9d25aeb39ec
sheam <sheam@LAPTOP-KNQHLT0Q>
2020-01-24 12:47:50 +0000

    getting rid of errors

---
+++ BadFileException.java
@@ -0,0 +1,8 @@
+
+public class BadFileException extends Exception {
+
+	public BadFileException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- MetroMapParser.java
+++ MetroMapParser.java
@@ -1,5 +1,3 @@
-package ex3;
-
 
 import java.io.BufferedReader;
 import java.io.FileReader;
