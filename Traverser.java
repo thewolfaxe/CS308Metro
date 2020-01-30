628f5cfc90868d0dc15274813ade6bfd2679abd8
Shea McPherson SE2017 <shea.mcpherson.2017@uni.strath.ac.uk>
2020-01-30 21:49:39 +0000

    Upload New File

---
+++ Traverser.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class Traverser {
+	
+	private Graph graph;
+	private LinkedList<Station>[] route;
+	
+	public Traverser(Graph graph) {
+		
+	}
+	
+	public LinkedList<Station>[] getRoute(Graph graph){
+		return null ;
+	}
+}
