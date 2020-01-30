4a308467076f6cd9dc05dd81d8afadb7e632587d
Shea McPherson SE2017 <shea.mcpherson.2017@uni.strath.ac.uk>
2020-01-30 21:49:48 +0000

    Upload New File

---
+++ UserView.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class UserView {
+	
+	public UserView() {
+		
+	}
+	
+	public Station getCurrentNode() {
+		return null;
+	}
+	
+	public Station getDestination() {
+		return null;
+	}
+	
+	public void displayRoute(LinkedList<Station>[] route) {
+		
+	}
+}
