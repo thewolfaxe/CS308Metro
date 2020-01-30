c3a0ce014a775a6f70938264a2162e37b3894de3
Shea McPherson SE2017 <shea.mcpherson.2017@uni.strath.ac.uk>
2020-01-30 21:47:42 +0000

    Upload New File

---
+++ Controller.java
@@ -0,0 +1,26 @@
+public class Controller {
+	
+	private Station current;
+	private Station destination;
+	private Graph graph;
+	
+	public Controller() {
+		
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public Station getCurrent() {
+		return null;
+	}
+	
+	public Station getDestination() {
+		return null;
+	}
+	
+	public void getRoute(Graph graph) {
+		
+	}
+}
