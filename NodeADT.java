dabe0decf5e169ff39099a76f0a797b41146f5a3
Shea McPherson SE2017 <shea.mcpherson.2017@uni.strath.ac.uk>
2020-01-30 21:48:46 +0000

    Upload New File

---
+++ NodeADT.java
@@ -0,0 +1,22 @@
+public abstract class NodeADT {
+	
+	private NodeADT before;
+	private NodeADT after;
+	private String id;
+	
+	public NodeADT() {
+		
+	}
+	
+	public NodeADT getBefore() {
+		return null;
+	}
+	
+	public NodeADT getAfter() {
+		return null;
+	}
+	
+	public String getID() {
+		return null;
+	}
+}
